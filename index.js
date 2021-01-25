5ea09a8de0f9ddd56d2b5fe5628bbe6ababf36f6
nitish amraji <nitish@nitishs-MacBook-Pro.local>
2021-01-24 19:49:35 -0600

    test

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const path = require('path')
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// // Serve static files from the React frontend app
+// app.use(express.static(path.join(__dirname, 'client/build')))
+// // Anything that doesn't match the above, send back index.html
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
 
 console.log('dir name: ' + __dirname);
 
@@ -49,6 +49,16 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
     next(err)
   }
 })
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
