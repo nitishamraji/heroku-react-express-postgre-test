b3945ef52628c92ed6647503591bfa5adac03861
nitish amraji <nitish@nitishs-MacBook-Pro.local>
2021-01-25 21:59:48 -0600

    deploy issue

--- server/sequelize/models/index.js
+++ server/sequelize/models/index.js
@@ -6,10 +6,13 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
+
 const db = {};
 
 console.log('.env file: ' + process.env.DATABASE_URL);
+console.log('config: ' + config.dialect);
 let sequelize = new Sequelize(process.env.DATABASE_URL, config);
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
