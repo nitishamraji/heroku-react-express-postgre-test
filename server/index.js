ca590330e6bd95ac7d09fb10a48f0eb0ca534ece
nitish amraji <nitish@nitishs-MacBook-Pro.local>
2021-01-25 21:20:29 -0600

    deploy issue

--- server/index.js
+++ server/index.js
@@ -139,6 +139,16 @@ console.log('connectionString: ' + process.env.DATABASE_URL);
 //   res.send("success");
 // })
 
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Mixing it up on port ${PORT}`)
